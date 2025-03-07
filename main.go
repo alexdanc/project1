521cbd92075b0e9ac606cc169f0dc67a4748bbf7
alexdanc <alex.danc@mail.ru>
2025-03-07 22:19:14 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,43 +1,51 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 type RequestBody struct {
-	Task string `json:"Hellow"`
+	Task string `json:"Hello"`
 }
 
-type Response struct {
-	Status      string `json:"status"`
-	RequestBody string `json:"body"`
-}
-
-var task []RequestBody
-
-func GetHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, &task)
-}
-
-func PostHandler(c echo.Context) error {
-	var tasks RequestBody
-	if err := c.Bind(&tasks); err != nil {
-		return c.JSON(http.StatusBadRequest, Response{
-			Status:      "failed",
-			RequestBody: " Don't Body",
-		})
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	var tasks []Task
+	result := DB.Find(&tasks)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
 	}
-	task = append(task, tasks)
-	return c.JSON(http.StatusOK, Response{
-		Status:      "success",
-		RequestBody: "Body Added",
-	})
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	result := DB.Create(&task)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(task)
+
 }
 
 func main() {
-	e := echo.New()
-	e.GET("/get", GetHandler)
-	e.POST("/post", PostHandler)
-	e.Logger.Fatal(e.Start(":8080"))
+	InitDB()
+
+	DB.AutoMigrate(&Task{})
+
+	router := mux.NewRouter()
+	router.HandleFunc("/task/get", GetHandler).Methods("GET")
+	router.HandleFunc("/task/post", PostHandler).Methods("POST")
+	http.ListenAndServe(":8080", router)
 }
